91116b0acafe2b674e7235737387f79087b2f771
Noah Fairbairn <noahfairbairn@gmail.com>
2019-06-10 16:24:44 -0700

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,32 @@
+class StudentsController < ApplicationController
+
+    def index
+        @students = Student.all
+    end
+    
+    def new
+        @student = Student.new
+    end
+    
+    def create
+        @student = Student.create(params.require(:student).permit(:first_name, :last_name))
+        redirect_to student_path(@student)
+    end
+    
+    def show
+        @student = Student.find(params[:id])
+    end
+    
+    def edit
+        @student = Student.find(params[:id])
+        render new_student_path
+    end
+    
+    def update
+        @student = Student.find(params[:id])
+        @student.update(params.require(:student).permit(:first_name, :last_name))
+        redirect_to student_path(@student)
+    end
+
+
+end
